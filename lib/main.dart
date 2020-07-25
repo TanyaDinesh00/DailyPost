d8ffffa4da5ac3b22b0dae7855e75454c320be30
TanyaDinesh00 <tanyadinesh21@gmail.com>
2020-07-25 13:15:46 +0530

    Initial commit.

---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+
+String url = "https://sahaysdailypost.com/";
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DailyPost',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      routes: {
+        "/": (context) => Home(),
+      },
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  HomeState createState() => HomeState();
+}
+
+class HomeState extends State<Home> {
+  final webView = FlutterWebviewPlugin();
+
+  @override
+  void initState() {
+    super.initState();
+    //webView.close();
+  }
+
+  @override
+  void dispose() {
+    webView.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: WebviewScaffold(
+        url: url,
+        withJavascript: true,
+        withLocalStorage: true,
+        withZoom: true,
+      ),
+    );
+  }
+}
